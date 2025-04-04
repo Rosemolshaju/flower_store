9dcf890af8e52d2ae0cdf3820c12ef1f7385d70e
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-04 12:14:10 +0100

    Create models.py

---
+++ backend/app/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from .database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+
+class Flower(Base):
+    __tablename__ = "flowers"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    image_url = Column(String)
+    rating = Column(Float)
