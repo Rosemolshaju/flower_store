d0f66577dc8cc9b9841f1f9bab4bf77775192506
Rosemol Shaju <93700438+Rosemolshaju@users.noreply.github.com>
2025-04-04 12:20:18 +0100

    Create login.py

---
+++ frontend/login.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Flower Shop - Login</title>
+</head>
+<body>
+    <form id="loginForm">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Login</button>
+    </form>
+    <script src="./scripts/login.js"></script>
+</body>
+</html>
